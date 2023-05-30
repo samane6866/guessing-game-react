3843ed17b4b5353cf1daa898834ea59d4e1f09b1
samane6866 <samane.bahramian@gmail.com>
2023-05-30 14:29:37 +0200

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,69 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+function random() {
+  return Math.floor(Math.random() * 10 + 1);
+}
 function App() {
+  const [numberRandom, setNumberRandom] = useState(random());
+  const [valorInput, setValorInput] = useState(" ");
+  const [restTry, setRestTry] = useState(1);
+  const [previousGuess, setPreviousGuess] = useState([]);
+  const [message, setShowMessage] = useState(" ");
+
+  function handelValorIniciar() {
+    console.log(valorInput, numberRandom);
+    if (valorInput > numberRandom) {
+      setShowMessage("is too high");
+    } else if (valorInput < numberRandom) {
+      setShowMessage("is to low");
+    }
+
+    setRestTry(restTry - 1);
+    if (setRestTry === 0) {
+      setShowMessage("YOU CAN CONTUNUE ANY MORE");
+    }
+
+    setPreviousGuess([...previousGuess, valorInput]);
+
+    setValorInput(" ");
+  }
+
   return (
     <>
       <h1>Number guessing game</h1>
       <p>Try and guess a random number between 1 and 100.</p>
-      <p>You have 10 attempts to guess the right number.</p>
+      <p>You have {restTry} attempts to guess the right number.</p>
       <br />
       <div id="wrapper">
-        <label htmlFor="guessField" id="guess">Guess a number</label>
-        <input type="text" id="guessField" class="guessField" />
-        <button class="guessSubmit">Submit a Guess</button>
+        <label htmlFor="guessField" id="guess">
+          Guess a number
+        </label>
+        <input
+          type="text"
+          id="guessField"
+          max={100}
+          min={0}
+          classn="guessField"
+          value={valorInput}
+          onChange={(e) => setValorInput(e.target.value)}
+        />
+        <button onClick={handelValorIniciar} className="guessSubmit">
+          Submit a Guess
+        </button>
 
-        <div class="resultParas">
-          <p>Previous Guesses: <span class="guesses"></span></p>
-          <p>Guesses Remaining: <span class="lastResult">X</span></p>
-          <p class="lowOrHi"></p>
+        <div className="resultParas">
+          <p>
+            Previous Guesses:{" "}
+            <span className="guesses">{previousGuess.join(",")}</span>
+          </p>
+          <p>
+            Guesses Remaining: <span className="lastResult">{restTry}</span>
+          </p>
+          <p className="lowOrHi">{message}</p>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
